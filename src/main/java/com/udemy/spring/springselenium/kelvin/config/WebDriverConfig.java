1db49489c4b01934ae4f098eb2aeb5f8a6272a92
shamnitjsr <shamnitjsr@gmail.com>
2025-01-08 01:57:02 +0530

    Spring Boot Cucumber Project

---
+++ src/main/java/com/udemy/spring/springselenium/kelvin/config/WebDriverConfig.java
@@ -0,0 +1,48 @@
+package com.udemy.spring.springselenium.kelvin.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+import com.udemy.spring.springselenium.kelvin.annotation.LazyConfiguration;
+import com.udemy.spring.springselenium.kelvin.annotation.ThreadScopeBean;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//@Profile("!remote")
+@LazyConfiguration
+public class WebDriverConfig {
+	
+    @ThreadScopeBean
+    @ConditionalOnMissingBean
+    //@ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver firefoxDriver(){
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+	
+	
+
+//    @ThreadScopeBean
+//    @ConditionalOnMissingBean
+//    //@ConditionalOnProperty(name = "browser", havingValue = "chrome")
+//    public WebDriver chromeDriver(){
+//        WebDriverManager.chromedriver().setup();
+//        return new ChromeDriver();
+//    }
+	
+	
+    
+//    @ThreadScopeBean
+//    @ConditionalOnMissingBean
+//    //@ConditionalOnProperty(name = "browser", havingValue = "edge")
+//    public WebDriver edgeDriver(){
+//        WebDriverManager.edgedriver().setup();
+//        return new EdgeDriver();
+//    }
+
+}
